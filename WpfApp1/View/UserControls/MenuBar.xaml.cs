6a05be75992c7b0a12466ac5e5c7f5ef09aac548
Link1515 <h26281660@gmail.com>
2024-10-18 11:08:43 +0800

    seperate userControls

---
+++ WpfApp1/View/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MenuBar.xaml
+    /// </summary>
+    public partial class MenuBar : UserControl
+    {
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
