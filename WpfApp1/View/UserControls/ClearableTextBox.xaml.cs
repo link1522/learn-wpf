e2c98e7de6ee7cf1ff0fa3ace89c7c691a03562d
Link1515 <h26281660@gmail.com>
2024-10-18 14:06:07 +0800

    create ClearableTextBox

---
+++ WpfApp1/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                // not a good way
+                tbPlaceholder.Text = value;
+            }
+        }
+
+
+        private void btnClear_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+                tbPlaceholder.Visibility = Visibility.Visible;
+            else
+                tbPlaceholder.Visibility = Visibility.Collapsed;
+        }
+    }
+}
