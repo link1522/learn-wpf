8c2b34c440353f1caff006375dc080190fcedf5e
Link1515 <h26281660@gmail.com>
2024-10-22 15:44:08 +0800

    learn data binding

---
+++ WpfApp1/View/UserControls/SetTextInput.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace WpfApp1.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SetTextInput.xaml
+    /// </summary>
+    public partial class SetTextInput : UserControl, INotifyPropertyChanged
+    {
+        public SetTextInput()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set { 
+                boundText = value; 
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+            }
+        }
+
+        private void btnSet_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            BoundText = "Set by button";
+        }
+    }
+}
