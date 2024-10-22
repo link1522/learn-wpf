67259dbba94d1faf4d1686d6aa9623d6703bcd10
Link1515 <h26281660@gmail.com>
2024-10-22 16:13:24 +0800

    learn MessageBox

--- WpfApp1/View/UserControls/MenuBar.xaml.cs
+++ WpfApp1/View/UserControls/MenuBar.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfApp1.View.UserControls
 {
@@ -10,6 +11,16 @@ namespace WpfApp1.View.UserControls
         public MenuBar()
         {
             InitializeComponent();
+        }
+
+        private void btn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show("Do you agree?", "agreement", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Success!");
+            }
         }
     }
 }
