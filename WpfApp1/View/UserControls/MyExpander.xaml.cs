03e255df51548b16cdb70511f7a8afe366f1c910
Link1515 <h26281660@gmail.com>
2024-10-30 16:55:11 +0800

    learn Expander

---
+++ WpfApp1/View/UserControls/MyExpander.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.View.UserControls
+{
+    public partial class MyExpander : UserControl
+    {
+        public MyExpander()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDetails_Click(object sender, RoutedEventArgs e)
+        {
+            expanderDetails.IsExpanded = !expanderDetails.IsExpanded;
+        }
+    }
+}
